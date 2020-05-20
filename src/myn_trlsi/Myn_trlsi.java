def6656c5c214ffbecf6385908a56ec2e9eceb3b
DilekEdagunduz <36606579+DilekEdagunduz@users.noreply.github.com>
2020-05-20 18:40:38 +0300

    deneme

---
+++ src/myn_trlsi/Myn_trlsi.java
@@ -0,0 +1,12 @@
+
+package myn_trlsi;
+ 
+public class Myn_trlsi {
+
+    public static void main(String[] args) {
+        form frm =new form();
+        frm.setExtendedState(javax.swing.JFrame.MAXIMIZED_BOTH);
+        frm.show();
+    }
+    
+}
